c78ae95a770ce69489f76e03023060c18e4422f2
KameronsCodeSpace <kameronmontague@pursuit.org>
2020-04-06 21:20:39 -0400

    Fixed React structure issues with landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
 
 // main pages
@@ -11,42 +11,18 @@ import UserProfile from './Components/Pages/UserProfile';
 
 // supporting jsx files
 // import Nav from './Components/Support Files/Nav'
-import Stories from './Components/Support Files/Stories';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 //Setting up a basic nav bar on each page. We can modify this structure later to
-//Work better with Authtication and Redux
+//Works better with Authtication and Redux
 const App = () => {
-
-  const [randomData, setData] = useState([]);
-  const [userName, setName] = useState([]);
-  const [storyTitle, setTitle] = useState([]);
-  const [storyImg, setImage] = useState([])
-
-  useEffect(() => {
-    (async function fetchData() {
-      // possible await function for some api
-      const response = ['Moscow', 'New York']
-      console.log('Get this info', response)
-      setData(response);
-      //some more examples of using hooks and props 
-      setName(['Akio Ostuka', 'Drake Anderson']);
-      setTitle(['Metal Gear', 'Online Persona']);
-      setImage(['https://images.squarespace-cdn.com/content/v1/58d66569b3db2b57ce7db1e2/1522601313393-6K0OVDA8KDS7MI048M4G/ke17ZwdGBToddI8pDm48kMm4aW_fDERSvfxGrBsAvC0UqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8PaoYXhp6HxIwZIk7-Mi3Tsic-L2IOPH3Dwrhl-Ne3Z2m3zK3svLWLcrfaJ51GFsfpc9KBPNtX5GL6Lnf5Th8F86liCGkj4dr9PBmyqqYlee/V+MGS01+copy.jpg?format=750w', 
-    'https://www.lovethispic.com/uploaded_images/107692-Colorful-Ocean-Wave.jpg'])
-    })();
-  }, []);
 
   return (
     <Router>
       <div className="App">
         {/* <Nav /> */}
-        {/* replace this <br /> with CSS */}
-        <Landing />
-        <Stories randomData={randomData[0]} userName={userName[0]} storyTitle={storyTitle[0]} storyImg={storyImg[0]} />
-        <Stories randomData={randomData[1]} userName={userName[1]} storyTitle={storyTitle[1]} storyImg={storyImg[1]} />
-        {/* <Switch>
+        <Switch>
 
           <Route exact path='/' component={Landing} />
           <Route path='/login' component={Login} />
@@ -55,7 +31,7 @@ const App = () => {
           <Route path='/trending' component={Trending} />
           <Route path='/userprofile' component={UserProfile} />
 
-        </Switch> */}
+        </Switch>
       </div>
     </Router>
   )
---
+++ frontend/src/Components/Support Files/AppTitlePage.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import background from '../../img/bg-shape.svg'
+
+
+function AppTitlePage() {
+    return (
+        <div>
+            <img src={background} alt='Background Shape' id='bg' />
+            <h1>Cover<br />To <span>Cover</span></h1>
+            <hr />
+            <p>Explore the stories of people all over the world <br />and find out what makes you special @ Group 6</p>
+            <div className='bottom'>
+                <div className='buttons'>
+                    <div className='btn'>
+                        <button>L</button>
+                        <p>Login</p>
+                    </div>
+                    <div className='btn'>
+                        <button>E</button>
+                        <p>Explore</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AppTitlePage;
