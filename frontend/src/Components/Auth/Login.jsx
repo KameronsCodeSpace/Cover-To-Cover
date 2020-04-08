d1d2a5827b15b49938fa9e3648be15a91486d386
KameronsCodeSpace <kameronmontague@pursuit.org>
2020-04-07 21:43:54 -0400

    Missed a div. Better starting login page

--- frontend/src/Components/Auth/Login.jsx
+++ frontend/src/Components/Auth/Login.jsx
@@ -12,30 +12,31 @@ class Login extends Component {
                     <div className={'imageTextBottom'}>Build it</div>
                 </div>
                 <div className={'rightBox'}>
-                    <div className={'box'}></div>
-                    <div className={'titleAuth'}>Log Into Cover to Cover</div>
-                    <div className={'inputSBox'}>
-                        <input className={'inputS'} type={'text'} placeholder={'Username'} />
-                    </div>
-                    <div className={'inputSBox'}>
-                        <input className={'inputS'} type={'password'} placeholder={'Password'} />
-                    </div>
-                    <div className={'contentBox'}>
-                        <div className={'checkboxBox'}>
-                            <input type={'checkbox'} className={'checkbox'} />
-                            <label className={'checkboxLabel'}>Remember</label>
+                    <div className={'box'}>
+                        <div className={'titleAuth'}>Log Into Cover to Cover</div>
+                        <div className={'inputSBox'}>
+                            <input className={'inputS'} type={'text'} placeholder={'Username'} />
                         </div>
-                        <div className={'text1'}>Forgot Password?</div>
-                    </div>
-                    <div className={'btnAuth'}>Login</div>
-                    <div className={'borderBox'}>
-                        <div className={'line'} />
-                        <div className={'text2 or'}>OR</div>
-                    </div>
-                    <div className={'socialMediaBox'}>
-                        <div className={'icAuth google'} />
-                        <div className={'icAuth facebook'} />
-                        <div className={'icAuth twitter'} />
+                        <div className={'inputSBox'}>
+                            <input className={'inputS'} type={'password'} placeholder={'Password'} />
+                        </div>
+                        <div className={'contentBox'}>
+                            <div className={'checkboxBox'}>
+                                <input type={'checkbox'} className={'checkbox'} />
+                                <label className={'checkboxLabel'}>Remember</label>
+                            </div>
+                            <div className={'text1'}>Forgot Password?</div>
+                        </div>
+                        <div className={'btnAuth'}>Login</div>
+                        <div className={'borderBox'}>
+                            <div className={'line'} />
+                            <div className={'text2 or'}>OR</div>
+                        </div>
+                        <div className={'socialMediaBox'}>
+                            <div className={'icAuth google'} />
+                            <div className={'icAuth facebook'} />
+                            <div className={'icAuth twitter'} />
+                        </div>
                     </div>
                 </div>
             </div>
