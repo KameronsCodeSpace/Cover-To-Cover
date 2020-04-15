b687af57dc111f8451a27885aa16beda32a503a7
RougeLight <dantaeflowers@pursuit.org>
2020-04-14 20:34:01 -0400

    sweeping backend and database fixes

---
+++ frontend/src/Components/posts.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import axios from 'axios';
+import './css-files/ItemUpload.css';
+
+class Item extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      image: null,
+    }
+  }
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleFileInput = async (event) => {
+    this.setState({
+      image: event.target.files[0],
+    })
+  }
+
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    const data = new FormData();
+    data.append('image', this.state.image);
+    try {
+      const res = await axios.post('/upload', data);
+      const post = axios.post(`/blog`, { })
+    } catch (err) {
+      console.log(err)
+    }
+
+  }
+
+
+
+export default Item;
