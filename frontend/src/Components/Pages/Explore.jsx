2803c88419cf95c4ea6423a8936aa989df5c69da
KameronsCodeSpace <kameronmontague@pursuit.org>
2020-05-02 00:25:51 -0400

    added the css for the blog story card

--- frontend/src/Components/Pages/Explore.jsx
+++ frontend/src/Components/Pages/Explore.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import axios from "axios";
 
 import Navbar from '../Support Files/Navbar'
+import staticPostImg from '../../img/theTower.jpeg';
 
 class Explore extends React.Component {
     constructor() {
@@ -29,17 +30,24 @@ class Explore extends React.Component {
         const { data } = this.state;
         console.log("render method data:", data);
         return (
-            <div id="stories_explore">
+            <div>
                 <Navbar />
 
                 {
 
                     data.map(element => {
                         return (
-                            <div className='blog'>
-                                <p>{element.id}</p>
-                                <p>{element.p_username}</p>
-                                <p>{element.caption}</p>
+                            <div className='blog-post'>
+                                <div className="blog-box">
+                                    <div className="blog-img">
+                                    <img src={staticPostImg} alt='img' />
+                                    </div>
+                                    <div className="blog-content">
+                                        {/* <p>{element.id}</p> */}
+                                        <h3>{element.p_username}</h3>
+                                        <p>{element.caption}</p>
+                                    </div>
+                                </div>
                             </div>
                         );
                     })
