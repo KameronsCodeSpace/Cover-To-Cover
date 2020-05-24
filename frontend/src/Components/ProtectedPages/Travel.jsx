c25611a0f6c8d5e28720e8f979f2ab96ec8b824c
jenama <johanneenama@gmail.com>
2020-05-24 13:50:19 -0400

    save

--- frontend/src/Components/ProtectedPages/Travel.jsx
+++ frontend/src/Components/ProtectedPages/Travel.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { login } from '../../Actions/authActions';
 import Navbar from '../Support Files/Navbar'
+const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 function Travel() {
+    mapboxgl.accessToken = 'pk.eyJ1IjoiamFlYjEwIiwiYSI6ImNrYWJleHZ0MTFicW8ycnFrY2RzdGcyM3EifQ.0b4pmg6kf_SSErXD4FTroQ';
+    const map = new mapboxgl.Map({
+        container: 'global',
+        style: 'mapbox://styles/mapbox/streets-v11'
+    });
     return (
-        <div>
+        <div className='global'>
             <Navbar />
             <div className="inner-pages">
                 <h1>Travel Page</h1>
@@ -13,4 +20,12 @@ function Travel() {
     );
 }
 
-export default Travel;
+
+const mapStateToProps = (state, ownProps) => {
+    console.log('check state:', state)
+    return state.auth.payload
+}
+
+
+export default (connect(mapStateToProps, { login })(Travel));
+
