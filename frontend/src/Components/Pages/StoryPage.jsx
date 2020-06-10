f832640658a47847e70124955c831c951cff847e
KameronsCodeSpace <kameronmontague@pursuit.org>
2020-06-10 17:14:21 -0400

    Added Link back to story page

--- frontend/src/Components/Pages/StoryPage.jsx
+++ frontend/src/Components/Pages/StoryPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from "axios";
+import { Link } from 'react-router-dom'
 
 import Navbar from '../Support Files/Navbar'
 import questionAvatar from '../../img/QuestionAvatar.png'
